c42c25d64aef78480daba3e746eb9073a1be848a
Wálysson Silva <walysson_silva@hotmail.com>
2021-03-27 16:07:31 -0300

    Inserindo incrementador

--- Semana5_Q1.js
+++ Semana5_Q1.js
@@ -3,13 +3,14 @@ function createList(quantidadeLi, texto) {
     console.log("criando a lista");
     var quantidadeLi = Number(window.prompt("Quantas linhas você deseja criar? "));//inserir número de linhas
     console.log(quantidadeLi);
-    var texto = String(window.prompt("Qual texto desejar colocar nas linhas? "));//inserir texto desejável 
+    var texto = (window.prompt("Qual texto desejar colocar nas linhas? "));//inserir texto desejável 
     console.log(texto);
-    
+    var textoNum = 0;
 
     for(var i = 0; i < quantidadeLi; i++) {
+        textoNum++;
         var node = document.createElement("li");                 // Create a <li> node
-        var textnode = document.createTextNode(texto);         // Create a text node
+        var textnode = document.createTextNode(`${texto} ${textoNum}`);         // Create a text node
         node.appendChild(textnode);                              // Append the text to <li>
         document.getElementById("area").appendChild(node);     // Append <li> to <ul> with id="myList"
         }
